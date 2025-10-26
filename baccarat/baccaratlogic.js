7f48a341acc64b2fd8394057955fda1f87584567
canvas-ku <jackkmorice@gmail.com>
2025-10-26 16:44:40 -0500

    fixed home page and respelt baccarat sorry alex

---
+++ baccarat/baccaratlogic.js
@@ -0,0 +1,6 @@
+//baccarat.js
+
+/* EECS 581: Group 21 Project 3
+ * The JavaScript Section for bacurat
+ * of Codesino.
+ */
--- bacurat/bacuratlogic.js
+++
@@ -1,6 +0,0 @@
-//bacurat.js
-
-/* EECS 581: Group 21 Project 3
- * The JavaScript Section for bacurat
- * of Codesino.
- */
