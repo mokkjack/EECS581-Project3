3c9c505b36e12202cf9a940b4ca06f88a0754a89
acarrillo13 <a690c665@home.ku.edu>
2025-11-01 20:35:29 -0500

    baccarat js outline

--- baccarat/baccaratlogic.js
+++ baccarat/baccaratlogic.js
@@ -4,3 +4,76 @@
  * The JavaScript Section for bacurat
  * of Codesino.
  */
+const decks = 5 
+const suits = ['Hearts', 'Diamonds', 'Clubs', 'Spades'];
+const values = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King', 'Ace'];
+class Shoe {
+    constructor() {
+        this.cards = [];
+        this.build();
+        this.shuffle();
+    }
+
+    build() {
+        this.cards = [];
+        for (let d = 0; d < decks; d++) {
+            for (const suit of suits) {
+                for (const value of values) {
+                    this.cards.push({ suit, value });
+                }
+            }
+        }
+    }
+
+    shuffle() {
+        for (let i = this.cards.length - 1; i > 0; i--) {
+            const j = Math.floor(Math.random() * (i + 1));
+            [this.cards[i], this.cards[j]] = [this.cards[j], this.cards[i]];
+        }
+    }
+
+    deal() {
+        if (this.cards.length === 0) {
+            this.build();
+            this.shuffle();
+        }
+        return this.cards.pop();
+    }
+}
+
+class BaccaratGame {
+
+    score(hand) {
+
+    }
+
+    thirdcard() {
+
+    }
+
+    winner() {
+
+    }
+
+    game() {
+
+    }
+}
+
+class Betting {
+    placeBet(type, amount){
+
+    }
+
+    Payout(winner){
+
+    }
+    
+    clearBet(){
+
+    }
+
+    doubleBet(){
+
+    }
+}
