0bd3df1f842eff04a9e7f31a549f8c2fff6819df
adcarrillo13 <a690c665@ku.edu>
2025-11-06 14:22:55 -0600

    fleshing out html and js for baccarat

--- baccarat/baccaratlogic.js
+++ baccarat/baccaratlogic.js
@@ -1,12 +1,25 @@
 //baccarat.js
 
 /* EECS 581: Group 21 Project 3
- * The JavaScript Section for bacurat
+ * The JavaScript Section for baccurat
  * of Codesino.
  */
 const decks = 5 
 const suits = ['Hearts', 'Diamonds', 'Clubs', 'Spades'];
 const values = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King', 'Ace'];
+
+document.addEventListener("DOMContentLoaded", () => {
+  let balanceDisplay = document.getElementById("balance");
+  balanceDisplay.textContent = GoonCoin;
+  playBtn.addEventListener("click", BaccaratGameGame);
+  playBtn.disabled = true;
+});
+
+function updateBalance() {
+  document.getElementById("balance").textContent = GoonCoin;
+  saveGoonCoin();
+}
+
 class Shoe {
     constructor() {
         this.cards = [];
